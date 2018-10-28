73dff499cf50391a3d9466d2414d4f211658f236
Ceiro Kilp <ceiro.kilp@gmail.com>
2018-10-28 15:55:17 +0200

    Add external storage feature to app

--- app/src/main/java/com/example/ceiro/rpsapp/ScoreActivity.java
+++ app/src/main/java/com/example/ceiro/rpsapp/ScoreActivity.java
@@ -1,8 +1,14 @@
 package com.example.ceiro.rpsapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +16,15 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,11 +36,15 @@ public class ScoreActivity extends AppCompatActivity {
     String playerName;
     Integer playerScore;
 
-    SharedPreferences.Editor editor;
-
     ArrayList<String> listItems = new ArrayList<String>();
-    ArrayList<String> tempArray = new ArrayList<String>();
     ArrayAdapter<String> adapter;
+
+    TextView currentText;
+
+    String line = null;
+
+    private static final int REQUEST_CODE_PERMISSION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,22 +54,25 @@ public class ScoreActivity extends AppCompatActivity {
         playAgain = (Button) findViewById(R.id.playAgain);
         playerName = getIntent().getStringExtra("player.name");
         playerScore = getIntent().getIntExtra("player.score", 0);
+        currentText = (TextView) findViewById(R.id.currentText);
 
-        SharedPreferences playerData = getSharedPreferences("playerInfo", Context.MODE_PRIVATE);
-        editor = playerData.edit();
-        editor.putString("playerInfo", playerName + "                                                     " + playerScore + "");
-        editor.apply();
-        editor.commit();
-        Map<String, ?> allEntries = playerData.getAll();
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
         list.setAdapter(adapter);
 
-        for(Map.Entry<String, ?> entry : allEntries.entrySet()){
-            listItems.add(entry.getValue().toString());
-        }
+        currentText.setText(playerName + "                                              " + playerScore);
 
         adapter.notifyDataSetChanged();
 
+        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(ScoreActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ScoreActivity.this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE }, REQUEST_CODE_PERMISSION);
+        }
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File externalDirectory = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)));
+            ShowDirectoryFilesInList(externalDirectory);
+        }
 
         playAgain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,5 +81,61 @@ public class ScoreActivity extends AppCompatActivity {
                 startActivity(playMore);
             }
         });
+    }
+
+    private boolean isExternalStorageWriteable() {
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.i("State", "Writeable");
+            return true;
+        } else return false;
+    }
+
+    public void onSave(View view) {
+        if(isExternalStorageWriteable()) {
+            File textFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+                    playerName + "                                                     " + playerScore + "");
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(textFile);
+                fileOutputStream.write((playerName + "                                                     " + playerScore).getBytes());
+                fileOutputStream.close();
+                Toast.makeText(this, "Score saved", Toast.LENGTH_SHORT).show();
+                currentText.setText("");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else Toast.makeText(this, "External storage is not mounted", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onShow(View view) {
+        if(isExternalStorageWriteable()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                File textFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+                        playerName + "                                                     " + playerScore + "");
+                FileInputStream fileInputStream = new FileInputStream(textFile);
+                if(fileInputStream != null) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    while((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line + "\n");
+                    }
+                    fileInputStream.close();
+                    currentText.setText(stringBuilder);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else Toast.makeText(this, "Error reading from external storage", Toast.LENGTH_SHORT).show();
+    }
+
+    public void ShowDirectoryFilesInList(File externalDirectory) {
+        File listFile[] = externalDirectory.listFiles();
+        if(listFile != null) {
+            for(int i = 0; i < listFile.length; i++) {
+                if(listFile[i].isDirectory()) {
+                    ShowDirectoryFilesInList(listFile[i]);
+                } else listItems.add(listFile[i].getName());
+            }
+        }
     }
 }
